84dff5f78994bc2d867b8b9f216787fb4f913e40
Sodabeh Sherzad <sodabehsherzad2018@gmail.com>
2023-02-14 11:58:42 +0100

    Saving data into cloud firestore

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 class AuthService {
 
   final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+  User? get getCurrentUser => _firebaseAuth.currentUser;
 
   Future createUserWithEmailAndPassword({required String email, required String password}) async {
     return await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
